a87fb7449cf08cd268c351c4aca725caba706b69
Ramya1235 <94964807+Ramya1235@users.noreply.github.com>
2022-03-17 09:54:38 +0530

    Add files via upload

---
+++ client/src/components/Navlogout.js
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+
+
+const Navlogout = () => {
+   
+    const { auth } = useSelector((state) => ({ ...state }));
+    const history = useHistory()
+    const logOut = () => {
+        
+        window.localStorage.removeItem('auth');
+        history.push("/");
+    }
+console.log(auth.user.isAdmin)
+
+
+    
+    return (
+
+        <div className="nav">
+            <a className='btn btn-secondary' onClick={logOut}>Logout</a>
+
+            {auth === null && (
+                <>
+                    <Link className='btn btn-info' to="/">Login</Link>
+                </>
+            )}
+        </div>
+
+
+    )
+};
+
+
+export default Navlogout;
